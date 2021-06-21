3237966b796950176990b1ca2d71d5b48521ba3a
Charlie Aleck <aleckcharlie@gmail.com>
2021-06-20 23:13:05 -0500

    updated server

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const fs = require('fs');
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let notes = [];
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db/db.json')));
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data))
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    notes.push(newNote);
+    writeFileAsync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
+    res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    notes = notes.filter((note) => note.id !== req.params.id);
+    writeFileAsync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
+    res.sendStatus(200);
+});
+
+
+// Starts the server to begin listening
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
