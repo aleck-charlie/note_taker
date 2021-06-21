22e366367619c04e44963ee33187430189625c93
Charlie Aleck <aleckcharlie@gmail.com>
2021-06-21 11:45:39 -0500

    troubleshoting JSON parse

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.static('public'));
 
 let notes = [];
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db/db.json')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
 
 app.get('/api/notes', (req, res) => {
     fs.readFile("./db/db.json", (err, data) => {
@@ -26,16 +26,17 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
+    let newNote = req.body;
     newNote.id = uuidv4();
     notes.push(newNote);
     writeFileAsync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
-    res.json(newNote);
+    res.json(JSON.parse(newNote));
+    
 });
 
 app.delete('/api/notes/:id', (req, res) => {
     notes = notes.filter((note) => note.id !== req.params.id);
-    writeFileAsync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
+    writeFileAsync(path.join(__dirname, 'db/db.json'), JSON.stringify(JSON.parse(notes)));
 });
 
 
